0b957001d62fd9188c83f6fd33f81a5fd93447c5
Harley Eiss <ice.eiss@gmail.com>
2024-07-03 22:02:13 -0400

    landing page images and alignment completed

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 // ADD IMPORTS BACK FOR GRAPHS SECTION
-// import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
-// import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
-// import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
+import GrantRatesByOfficeImg from '../../../styles/Images/bar-graph-no-text.png';
+import GrantRatesByNationalityImg from '../../../styles/Images/pie-chart-no-text.png';
+import GrantRatesOverTimeImg from '../../../styles/Images/line-graph-no-text.png';
 import HrfPhoto from '../../../styles/Images/paper-stack.jpg';
 import '../../../styles/RenderLandingPage.less';
 import { Button } from 'antd';
@@ -32,7 +32,22 @@ function RenderLandingPage(props) {
       </div>
 
       {/* Graphs Section: Add code here for the graphs section for your first ticket */}
-      {/* <div className="graphs-section"> */}
+      <div className="graphs-section">
+        <div>
+          <img src={GrantRatesByOfficeImg} alt='bar-graph' className='bar-graph'/>
+          <p>Search Rates By Office</p>
+        </div>
+
+        <div>
+          <img src={GrantRatesByNationalityImg} alt='bar-graph' className='bar-graph'/>
+          <p>Search Grants By Nationality</p>
+        </div>
+
+        <div>
+          <img src={GrantRatesOverTimeImg} alt='bar-graph' className='bar-graph'/>
+          <p>Search Grant Rates Over Time</p>
+        </div>
+      </div>
       <div className="view-more-data-btn-container">
         <Button
           type="default"
