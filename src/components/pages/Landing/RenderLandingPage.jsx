7f77de0398ba2f2fd233193f0fd4a8261598c919
Harley Eiss <ice.eiss@gmail.com>
2024-07-23 13:19:34 -0400

    completed

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -53,8 +53,17 @@ function RenderLandingPage(props) {
           type="default"
           style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
           onClick={() => history.push('/graphs')}
+          className='view-button'
         >
           View the Data
+        </Button>
+        <Button
+          type="default"
+          style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+          onClick={() => history.push('/graphs')}
+          className='download-button'
+        >
+          Download the Data
         </Button>
       </div>
 
@@ -74,9 +83,46 @@ function RenderLandingPage(props) {
           </h3>
         </div>
       </div>
-      <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
+      <div className="bottom-section">
+          <h1>Systematic Disparity Insights</h1>
+        <div className='data-container'>
+          <div>
+            <h2>36%</h2>
+              <h3>
+              By the end of the Trump administration, the average 
+              asylum office grant rate had fallen 36 percent from an 
+              average of 44 percent in fiscal year 2016 to 28 percent 
+              in fiscal year 2020
+              </h3>
+          </div>
+          <div>
+            <h2>5%</h2>
+            <h3> 
+              The New York asylum office grant rate dropped to 5
+              percent in the fiscal year 2020.
+            </h3>
+          </div>
+          <div>
+            <h2>6x Lower</h2>
+            <h3>
+              Between fiscal year 2017 and 2020, the New York
+              asylum office's average grant rate was six times lower
+              than the San fancisco asylum office.
+            </h3>
+          </div>
+        </div>
+      <div className="read-more-data-btn-container">
+        <Button
+          type="default"
+          style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+          onClick={() => history.push('/graphs')}
+          className='read-button'
+        >
+          Read More
+        </Button>
+      </div>
+      
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
